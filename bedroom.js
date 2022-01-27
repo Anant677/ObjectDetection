2ac7f1e47d90f934427da5ac75073d9a14b9895c
Anant677 <80251092+Anant677@users.noreply.github.com>
2022-01-27 16:16:55 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,42 @@
+img = "";
+state = "";
+object = [];
+function preload(){
+img = loadImage("wooden-double-bed-500x500.png");
+}
+function setup(){
+canvas = createCanvas(400,400);
+canvas.center();
+objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+}
+function draw(){
+    image(img,0,0,400,400);
+if(state!= ""){
+   for(var i=0;i<object.length;i++){
+       document.getElementById("status").innerHTML = "There are 2 objects in the picture and it detected 1";
+       confidence = floor(object[i].confidence*100);
+       fill("#f71500");
+       text(object[i].label +" "+confidence+" "+"%" , object[i].x , object[i].y);
+       noFill();
+       stroke("#f71500");
+       rect(object[i].x , object[i].y , object[i].width+40 , object[i].height+40);
+   }
+}
+
+}
+//rect(x,y,width,height,radius);
+function modelLoaded(){
+    console.log("model is initialized");
+    state = true;
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    objectDetector.detect(img,gotResult);
+    }
+    function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        object = results;
+}
+
+}
---
+++ tv_ac.js
@@ -0,0 +1,41 @@
+img = "";
+state = "";
+object = [];
+function preload(){
+img = loadImage("666c191c19bea8d1a375662a70cbfe53.jpg");
+}
+function setup(){
+canvas = createCanvas(400,400);
+canvas.center();
+objectDetector = ml5.objectDetector("cocossd",modelLoaded);
+}
+function draw(){
+    image(img,0,0,400,400);
+    if(state!= ""){
+       for(var i=0;i<object.length;i++){
+           document.getElementById("status").innerHTML = "There are 2 objects in the picture and it detected 0";
+           confidence = floor(object[i].confidence*100);
+           fill("#f71500");
+           text(object[i].label +" "+confidence+" "+"%" , object[i].x , object[i].y);
+           noFill();
+           stroke("#f71500");
+           rect(object[i].x , object[i].y , object[i].width+40 , object[i].height+40);
+       }
+    }
+    
+    }
+//rect(x,y,width,height);
+function modelLoaded(){
+    console.log("model is initialized");
+    state = true;
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    objectDetector.detect(img,gotResult);
+    }
+    function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        object = results;
+    }
+    }
